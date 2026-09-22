e34052af1d6ce93cf767b64e5d42aaa8bb5cd789
IsmaelParedes2008 <iparedesn2@unemi.edu.ec>
2026-09-22 11:10:40 -0500

    Update Ejercicio_11.py

--- S3-Tarea_1/Ejercicio_11.py
+++ S3-Tarea_1/Ejercicio_11.py
@@ -90,3 +90,82 @@ while True:
 
         case _:
             print("Opción inválida. Intente de nuevo.")
+
+#EJERCICIO SIMILAR
+"""CONTADOR DE PRODUCTOS
+
+Crear una clase ContadorProductos que:
+
+agregar_producto(nombre) → guarde el producto en un diccionario contando cuántas veces se registra.
+producto_mas_registrado() → retorne el producto con mayor cantidad de registros.
+cantidad_producto(nombre) → retorne cuántas veces fue registrado."""
+class ContadorProductos:
+    def __init__(self):
+        self.productos = {}
+
+    def agregar_producto(self, nombre):
+        if nombre in self.productos:
+            self.productos[nombre] += 1
+        else:
+            self.productos[nombre] = 1
+
+    def producto_mas_registrado(self):
+        if len(self.productos) == 0:
+            return "No hay productos"
+
+        producto_mayor = ""
+        max_cantidad = -1
+
+        for producto, cantidad in self.productos.items():
+            if cantidad > max_cantidad:
+                max_cantidad = cantidad
+                producto_mayor = producto
+
+        return producto_mayor
+
+    def cantidad_producto(self, nombre):
+        if nombre in self.productos:
+            return self.productos[nombre]
+        else:
+            return 0
+
+resultado = ContadorProductos()
+
+while True:
+    print("\n=== CONTADOR DE PRODUCTOS ===")
+    print("1. Registrar productos")
+    print("2. Ver producto más registrado")
+    print("3. Consultar cantidad de un producto")
+    print("4. Ver diccionario")
+    print("5. Salir")
+
+    opcion = input("Ingrese una opción (1-5): ")
+
+    match opcion:
+        case "1":
+            entrada = input("Ingrese productos separados por espacio o coma: ")
+            productos = entrada.replace(",", " ").split()
+
+            for i in productos:
+                resultado.agregar_producto(i)
+
+            print("Productos registrados correctamente.")
+
+        case "2":
+            producto = resultado.producto_mas_registrado()
+            print(f"Producto más registrado: {producto}")
+
+        case "3":
+            nombre = input("Ingrese el producto que desea consultar: ")
+            cantidad = resultado.cantidad_producto(nombre)
+            print(f"El producto '{nombre}' aparece {cantidad} veces.")
+
+        case "4":
+            print(f"Diccionario: {resultado.productos}")
+
+        case "5":
+            print("=== CERRANDO PROGRAMA ===")
+            break
+
+        case _:
+            print("Opción inválida.")
